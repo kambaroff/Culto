598a7fb7f72cf35465d61124f8dbda42151928f1
Bekzat <kambarbekzat@gmail.com>
2019-05-02 18:52:14 +0600

    Added Quiz Fragment, will have to take and add data to Firebase Database.

--- app/src/main/java/kz/iitu/culto/SideMenuActivity.java
+++ app/src/main/java/kz/iitu/culto/SideMenuActivity.java
@@ -10,6 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import kz.iitu.culto.MenuFragments.AboutUsFragment;
 import kz.iitu.culto.MenuFragments.ProfileFragment;
@@ -20,10 +30,28 @@ public class SideMenuActivity extends AppCompatActivity implements NavigationVie
 
     private DrawerLayout drawer;
 
+    private DatabaseReference profileUserRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
+
+    private ImageView profilePhoto;
+
+    private TextView firstName;
+    private TextView lastName;
+    private TextView userEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_side_menu);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+
+        profileUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+
+
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -31,18 +59,54 @@ public class SideMenuActivity extends AppCompatActivity implements NavigationVie
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
         navigationView.setNavigationItemSelectedListener(this);
+
+        profilePhoto = headerView.findViewById(R.id.profile_round_picture);
+        firstName = headerView.findViewById(R.id.name_view);
+        lastName = headerView.findViewById(R.id.surname_view);
+        userEmail = headerView.findViewById(R.id.email_view);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        profileUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    String myFirstName = dataSnapshot.child("name").getValue().toString();
+                    String myLastName = dataSnapshot.child("surname").getValue().toString();
+                    String myEmail = dataSnapshot.child("email").getValue().toString();
+
+                    firstName.setText(myFirstName);
+                    lastName.setText(myLastName);
+                    userEmail.setText(myEmail);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         if(savedInstanceState == null){
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new ProfileFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_profile);}
     }
+
+    private void sendUserToLoginActivity(){
+        Intent loginIntent = new Intent(SideMenuActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+
+    }
+
 
 
     @Override
@@ -68,10 +132,13 @@ public class SideMenuActivity extends AppCompatActivity implements NavigationVie
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new ProfileFragment()).commit();
                 break;
-            case R.id.nav_tests:
+            case R.id.nav_quiz:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new QuizFragment()).commit();
                 break;
+            case R.id.nav_logout:
+                mAuth.signOut();
+                sendUserToLoginActivity();
         }
 
         drawer.closeDrawer(GravityCompat.START);
